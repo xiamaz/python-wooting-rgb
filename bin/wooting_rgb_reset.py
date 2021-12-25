bfe128a01428b3dcf109956796593b72b16f5bd1
Max Zhao <alcasa.mz@gmail.com>
2021-12-25 20:49:27 +0100

    Adding comment

--- bin/wooting_rgb_reset.py
+++ bin/wooting_rgb_reset.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""Reset the RGB colors on the keyboard back to predefined colors.
+"""
 from wooting_rgb import wooting_rgb_wrapper
 
 wooting_rgb_wrapper.wooting_rgb_reset()
